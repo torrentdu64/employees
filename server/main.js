b94ef4f9b9ef83a7d659051b2b7d7632ab75e367
torrent benoit <torrentdu64@gmail.com>
2017-09-22 16:55:21 +0200

    faker done

--- server/main.js
+++ server/main.js
@@ -1,10 +1,21 @@
+
+import _ from 'lodash';
 import { Meteor } from 'meteor/meteor';
 import { Employees } from '../imports/collections/employees';
+import { image, helpers} from 'faker';
 
 
 Meteor.startup( () => {
     const numberRecords = Employees.find({}).count();
     if (!numberRecords) {
-
+        _.time(5000, () => {
+              const {email, name, phone } = helpers.createCard();
+              Employees.insert({
+                  name,
+                  email,
+                  phone,
+                  avatar: image.avatar()
+              });
+        });
     }
 });
