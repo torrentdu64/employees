4dfa6ab6cf56b290ce9231df16d69d33ebd335b8
torrent benoit <torrentdu64@gmail.com>
2017-09-22 16:45:50 +0200

    implemente faker

--- server/main.js
+++ server/main.js
@@ -1,6 +1,10 @@
 import { Meteor } from 'meteor/meteor';
+import { Employees } from '../imports/collections/employees';
 
 
 Meteor.startup( () => {
+    const numberRecords = Employees.find({}).count();
+    if (!numberRecords) {
 
+    }
 });
