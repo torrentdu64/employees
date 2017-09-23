212e5a42ec75ba53d27afa92a6cfc8975f195e3c
torrent benoit <torrentdu64@gmail.com>
2017-09-23 15:29:33 +0200

    add bootstrap to the project

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -8,7 +8,8 @@ const EmployeeList = (props) => {
     return (
       <div>
         <div className="employee-list">
-          {props.employees.map(employee => <EmployeeDetail employee={employee} />)}
+          {props.employees.map(employee =>
+            <EmployeeDetail key={employee._id} employee={employee} />)}
         </div>
       </div>
       );
