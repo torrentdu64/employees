f7f2b4419977a96932fd903e08fa5938bd8ea303
torrent benoit <torrentdu64@gmail.com>
2017-09-23 14:13:26 +0200

    employee list

---
+++ client/components/employee_list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const EmployeeList = () => {
+    return (
+      <div>
+        <div className="employee-list">
+          employee list
+        </div>
+      </div>
+      );
+};
+
+
+export default EmployeeList;
