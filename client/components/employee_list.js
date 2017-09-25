bcc6bca860a3e435f2c9e0576892acad20406db3
torrent benoit <torrentdu64@gmail.com>
2017-09-25 15:20:19 +0200

    component => class

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 import EmployeeDetail from './employee_detail';
 
 const PER_PAGE = 20;
 
-const EmployeeList = (props) => {
-  console.log(props.employees);
+class EmployeeList extends Component {
+  render() {
     return (
       <div>
         <div className="employee-list">
-          {props.employees.map(employee =>
+          {this.props.employees.map(employee =>
             <EmployeeDetail key={employee._id} employee={employee} />)}
         </div>
         <button onClick={ () => Meteor.subscribe('employees', 40)   }
@@ -19,6 +19,7 @@ const EmployeeList = (props) => {
         </button>
       </div>
       );
+  }
 };
 
 
