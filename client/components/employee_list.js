b26411c3f9d2f2aae0f127d1e344029cd0f28712
torrent benoit <torrentdu64@gmail.com>
2017-09-25 15:13:19 +0200

    add pagination

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -3,6 +3,8 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 import EmployeeDetail from './employee_detail';
 
+const PER_PAGE = 20;
+
 const EmployeeList = (props) => {
   console.log(props.employees);
     return (
@@ -11,12 +13,16 @@ const EmployeeList = (props) => {
           {props.employees.map(employee =>
             <EmployeeDetail key={employee._id} employee={employee} />)}
         </div>
+        <button onClick={ () => Meteor.subscribe('employees', 40)   }
+        className="btn btn-primary"
+        >Load more ...
+        </button>
       </div>
       );
 };
 
 
 export default createContainer(() => {
-    Meteor.subscribe('employees');
+    Meteor.subscribe('employees', PER_PAGE);
     return { employees: Employees.find({}).fetch()};
 },EmployeeList);
