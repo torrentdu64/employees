51362f674e6ed5f80e91e57af5f8665ae79ac496
torrent benoit <torrentdu64@gmail.com>
2017-09-25 15:32:31 +0200

    pagination done with componentWillMount

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -6,6 +6,17 @@ import EmployeeDetail from './employee_detail';
 const PER_PAGE = 20;
 
 class EmployeeList extends Component {
+
+  componentWillMount() {
+    this.page = 1;
+  }
+
+  handleButtonClick () {
+      Meteor.subscribe('employees', (PER_PAGE * this.page + 1));
+      this.page += 1;
+  }
+
+
   render() {
     return (
       <div>
@@ -13,7 +24,7 @@ class EmployeeList extends Component {
           {this.props.employees.map(employee =>
             <EmployeeDetail key={employee._id} employee={employee} />)}
         </div>
-        <button onClick={ () => Meteor.subscribe('employees', 40)   }
+        <button onClick={ this.handleButtonClick.bind(this)  }
         className="btn btn-primary"
         >Load more ...
         </button>
